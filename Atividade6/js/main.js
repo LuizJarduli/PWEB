84cf37f8c054eb141680faea466ee558417d7a25
LuizJarduli <luizjarduli@gmail.com>
2022-10-15 16:28:43 -0300

    feat: atividade6, operações matemáticas

--- Atividade6/js/main.js
+++ Atividade6/js/main.js
@@ -18,7 +18,7 @@ function getNome() {
     return prompt('Nome do aluno:')
 }
 
-function setResultado(nome, media) {
+function setResultadoMedia(nome, media) {
     const nomeContainer = document.getElementById('nome');
     const mediaContainer = document.getElementById('media');
 
@@ -29,10 +29,51 @@ function setResultado(nome, media) {
     mediaContainer.insertAdjacentText('beforebegin', `Média: ${parseFloat(media)?.toFixed(2)}`);
 }
 
-function onInit() {
+function setResultadoNoElemento(idDoElemento, resultado) {
+    const elemento = document.getElementById(idDoElemento);
+
+    elemento.insertAdjacentHTML('beforeend', `<p>${resultado}</p>`)
+}
+
+function getDoisNumeros() {
+    let qtdNums = 0;
+    let numeros = [];
+    while (qtdNums < 2) {
+        numeros.push(Number(prompt(`Digite o ${qtdNums + 1}`)));
+        qtdNums++;
+    }
+
+    return numeros;
+}
+
+function calcularEval(numeros = [], operacao = '*') {
+    const stringOperacao = numeros.reduce((prev, current) => `${prev}${operacao}${current}`);
+    return eval(stringOperacao);
+}
+
+function executarCalculos(numeros = []) {
+    // calculos
+    const soma = `Soma: ${calcularEval(numeros, '+')}`;
+    const subtracao = `Subtração: ${calcularEval(numeros, '-')}`;
+    const multiplicacao = `Multiplicação: ${calcularEval(numeros, '*')}`;
+    const divisao = `Divisão: ${calcularEval(numeros, '/')}`;
+    const resto = `Resto: ${calcularEval(numeros, '%')}`;
+
+    return [soma, subtracao, multiplicacao, divisao, resto];
+
+}
+
+function onInitMedia() {
     alert('Insira seu nome e notas para calcular a média aritmética');
     const nome = getNome();
     const notas = getNotas(3);
     const media = calcMedia(notas);
-    setResultado(nome, media);
+    setResultadoMedia(nome, media);
+}
+
+function onInitOperacoes() {
+    alert('Insira dois numeros para realizar operações de soma, subtração, multiplicação, divisão e calcular seu resto');
+    const numeros = getDoisNumeros();
+    const getCalculos = executarCalculos(numeros);
+    getCalculos.forEach((calculo) => setResultadoNoElemento('resultado', calculo));
 }
