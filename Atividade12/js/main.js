338ea626a728549c5c8c128886fe25e29db62e39
Luiz Miguel <luizjarduli@gmail.com>
2022-11-21 10:49:59 -0300

    docs: atividade 12

--- Atividade12/js/main.js
+++ Atividade12/js/main.js
@@ -1,19 +1,41 @@
 
 onInit();
 
+/**
+ * setOpenedWindow
+ *
+ * Troca a img de fundo para janela aberta
+ * @param element 
+ */
 function setOpenedWindow(element) {
     element.src = './img/janelaaberta.png';
 }
 
+/**
+ * setClosedWindow
+ *
+ * Troca a img de fundo para janela fechada
+ * @param element 
+ */
 function setClosedWindow(element) {
     element.src = './img/janelafechada.png';
 }
 
+/**
+ * setBrokenWindow
+ *
+ * Troca a img de fundo para janela quebrada
+ * @param element 
+ */
 function setBrokenWindow(element) {
     element.src = './img/janelaquebra.png';
 }
 
-
+/**
+ * onInit
+ *
+ * função que inicializa o script
+ */
 function onInit() {
     const getImageElement = document.querySelector('#application-wrapper > img');
 
